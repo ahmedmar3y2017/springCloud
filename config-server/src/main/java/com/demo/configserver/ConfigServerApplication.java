51a89976d6d61efa70f5067698c025524c14bbcf
AhmedMarey <ahmed.marey@asset.com.eg>
2024-11-29 21:00:01 +0200

    -- init

---
+++ config-server/src/main/java/com/demo/configserver/ConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.demo.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
---
+++ reservation-service/src/main/java/com/frankmoley/lil/reservationservice/data/ReservationEntity.java
@@ -0,0 +1,28 @@
+package com.frankmoley.lil.reservationservice.data;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+//import jakarta.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name = "RESERVATIONS")
+@Getter
+@Setter
+@NoArgsConstructor
+public class ReservationEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "RESERVATION_ID")
+    private long reservationId;
+    @Column(name = "ROOM_ID")
+    private long roomId;
+    @Column(name = "GUEST_ID")
+    private long guestId;
+    @Column(name = "RES_DATE")
+    private Date date;
+}
