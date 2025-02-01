908c27f6d82eeff47afc71898c5022bed0f2d3d6
AhmedMarey <ahmed.marey@asset.com.eg>
2025-02-01 13:54:38 +0200

    -- flyway schema versioning

---
+++ guest-service/src/main/resources/db/migration/V3__update_GUESTS_table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE  IF NOT EXISTS GUESTS
+(
+    GUEST_ID      SERIAL  PRIMARY KEY,
+    FIRST_NAME    VARCHAR(64),
+    LAST_NAME     VARCHAR(64),
+    EMAIL_ADDRESS VARCHAR(64),
+    ADDRESS       VARCHAR(64),
+    COUNTRY       VARCHAR(32),
+    STATE         VARCHAR(12),
+    PHONE_NUMBER  VARCHAR(24)
+);
+
+
+alter table GUESTS add column lastEdit varchar(200) ;
+
